793c407e81250fcf5c031cf5af6fe406a1a2ce34
Isira Adithya Weerasinghe <isira.aw@gmail.com>
2026-01-06 15:36:06 +0530

    cose error fixef

--- app/main.py
+++ app/main.py
@@ -13,7 +13,7 @@ app = FastAPI(title="Sri Lanka Weather Analytics API")
 # CORS middleware for frontend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000","https://srilankaweatherfrontend-production.up.railway.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
